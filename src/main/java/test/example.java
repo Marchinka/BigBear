affca6ebd5a3c32b134ea53732ad9bc60f027649
Marchinka <marc89rossi@gmail.com>
2017-03-12 19:21:03 +0100

    test example

---
+++ src/main/java/test/example.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class example {
+
+	@Test
+	public void test() {
+		assertEquals("10 x 0 must be 0", 0, 0);
+	}
+
+}
